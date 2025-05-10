7babb35e959ed611cdc47b633010d098bbde64f0
dathx <hxuandat2001@gmail.com>
2025-05-10 13:19:43 +0700

    sit change

--- src/main/java/com/webflux/controller/AccountRestController.java
+++ src/main/java/com/webflux/controller/AccountRestController.java
@@ -16,7 +16,7 @@ public class AccountRestController {
 
     private final AccountService accountService;
 
-
+    // SIT
     @GetMapping(value = "/account/{accountId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<AccountResponse> getAccount(@PathVariable("accountId") String accountId) {
         return accountService.findAccountById(accountId)
